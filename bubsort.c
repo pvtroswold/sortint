91ff6475891c3656bd8e05fbe0660661db6f79a0
pvtroswold <pvtroswold@users.noreply.github.com>
2019-03-04 13:55:17 -0600

    Update bubsort.c

--- bubsort.c
+++ bubsort.c
@@ -29,22 +29,23 @@ o main(o argc, o **argv)
 {
 	srand(time(NULL));
 	o *i;
-	o d=1000,v=0,q=0;
+	o d=1000,v=0,q=0,n=0;
 	double cl;
 	
 	for(int j=0;j<argc;j++)
 	{
 		if(strcmp(argv[j],"-s")==0 && argc>j) d=atoi(argv[++j]);
-		if(strcmp(argv[j],"-v")==0) v=1;
-		if(strcmp(argv[j],"-q")==0) q=1;
+		if(strcmp(argv[j],"-v")==0) v=1; //verbose
+		if(strcmp(argv[j],"-n")==0) v=1,n=1; //output values on each line
+		if(strcmp(argv[j],"-q")==0) q=1; //use qsort instead
 		if(strcmp(argv[j],"?")==0)
 		{
-			p("usage: %s [-s <size of array>] [-v (for verbose)] [-q (use qsort instead)] [?]\n",argv[0]);
+			p("usage: %s [?] [-s <size of array>] [-v (for verbose)] [-n (ints only output)] [-q (use qsort instead)]\n",argv[0]);
 			return 0;
 		}
 	}
 	
-	if(!q && d>=100000)
+	if(!n && !q && d>=100000)
 	{
 		q=1;
 		p("automatically using qsort since size>=100000\n");
@@ -57,18 +58,18 @@ o main(o argc, o **argv)
 		return 1;
 	}
 	
-	p("randomizing...\n");
+	if(!n)p("randomizing...\n");
 	for(int j=0;j<d;j++)i[j]=rand()%UINT_MAX;
 	
 	
 	// p("number of elements: %i\n",d);
-	if(v)
+	if(!n && v)
 	{
 		p("before: ");for(int j=0;j<d;j++)p("%i,",i[j]);
 		putc('\n',stdout);
 	}
 	
-	p("sorting %u elements...\n",d);
+	if(!n)p("sorting %u elements...\n",d);
 	cl=(double)clock();
 	
 	//-q to use qsort
@@ -76,18 +77,23 @@ o main(o argc, o **argv)
 		qsort(i,d,sizeof(int),s2);
 	else
 		for(int k=0;k<d-1;k++)
-			for(int j=0;j<d-1;j++)s(i+j);
+			for(int j=0;(j<d-1-k);j++)s(i+j);
 	
-	p("sorting done\n");
+	if(!n)p("sorting done\n");
 	cl=(double)clock()-cl; //'stop' clock
 	
 	if(v)
 	{
-		p("\nafter:  ");for(int j=0;j<d;j++)p("%i,",i[j]);
+		if(!n)p("\nafter:  ");
+		for(int j=0;j<d;j++)
+		{
+			if(!n)p("%i,",i[j]);
+			else p("%i\n",i[j]);
+		}
 		putc('\n',stdout);
 	}
 	
-	p("finished\nclocks: %u (%02u:%02u secs)\n",(uint64_t)(cl),(uint64_t)((double)(cl/CLOCKS_PER_SEC)/60.0L),(uint64_t)fmod(cl/CLOCKS_PER_SEC,60.0L));
+	if(!n)p("finished\nclocks: %u (%02u:%02u secs)\n",(uint64_t)(cl),(uint64_t)((double)(cl/CLOCKS_PER_SEC)/60.0L),(uint64_t)fmod(cl/CLOCKS_PER_SEC,60.0L));
 	
 	return 0;
 }
