ade19abbb3c1cbb5221988e0e67ff8877a21beb3
pvtroswold <pvtroswold@users.noreply.github.com>
2019-03-04 15:28:49 -0600

    Add files via upload
    This is a separate program that allows you to view bubsort.c output from a file. Depends on the Tigr graphics library.
    
    I.e., after building both programs, run "bubsort -n -s 700 & view"

---
+++ view.c
@@ -0,0 +1,59 @@
+#include "tigr.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#define xy2i(x,y,w) ((y)*w+(x))
+#define spix(x,y,w,col) wind->pix[xy2i((x),(y),w)*sizeof(TPixel)].r=col>>16&0x0000ff;\
+		wind->pix[xy2i((x),(y),w)*sizeof(TPixel)].g=col>>8&0x0000ff;\
+		wind->pix[xy2i((x),(y),w)*sizeof(TPixel)].b=col&0x0000ff
+
+void main()
+{
+	int *i,d;
+	
+	Tigr *wind=tigrWindow(640,480,"this heer ar winndo",NULL);
+	
+	tigrUpdate(wind);
+	printf("reading file...\n");
+	FILE *f=fopen("t.dat","rb");
+	assert(f!=NULL);
+	if(f)
+	{
+		fscanf(f,"ints: %i",&d);
+		assert(d!=0);
+		
+		i=malloc(d*sizeof(int));
+		assert(i!=NULL);
+		printf("read %i ints\n",d);
+		
+		for(int j=0;j<d;j++)
+		{
+			fscanf(f,"%i\n",&i[j]);
+			// printf("%i: %i\n",j,i[j]);
+		}
+	}
+	printf("successfully read file\n");
+	
+	printf("smallest:%i\nbiggest:%i\n",i[0],i[d-1]);
+	
+	while(true)
+	{
+		if(tigrClosed(wind) || tigrKeyHeld(wind,TK_ESCAPE)) break; //break out of loop
+		
+		for(int j=0;j<d;j++)
+		{
+			float ratio1=((float)j/d)*640.0f;
+			float ratio2=((float)(i[j])/35536.0f)*480.0f;
+			// printf("i[j]:%i,r2:%i\n",i[j],(int)ratio2);
+			tigrLine(wind,  (int)ratio1,480,  (int)ratio1,480-((int)ratio2),  tigrRGB(0xff,0xff,0xff));
+		}
+		
+		tigrUpdate(wind);
+		Sleep(20);
+	}
+	
+	free(i);
+	tigrFree(wind);
+}
