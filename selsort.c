8a4870e06476d8b9f970a052fe1d2b7dcd3a524b
pvtroswold <pvtroswold@users.noreply.github.com>
2019-03-04 12:33:07 -0600

    Update selsort.c

--- selsort.c
+++ selsort.c
@@ -1,4 +1,5 @@
 //selection sort variant
+#include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
 #include <inttypes.h>
@@ -26,54 +27,63 @@ int s2(int *a,int *b) //for qsort
 }
 o main(o argc, o **argv)
 {
+	srand(time(NULL));
+	o *i;
 	o d=1000,v=0,q=0;
+	
 	// if(argc<2)
 	// {
 		// p("usage: %s [-s <size of array>] [-v (for verbose)] [-q (use qsort instead)]\n",argv[0]);
 		// return 1;
 	// }
+	
 	for(int j=0;j<argc;j++)
 	{
 		if(strcmp(argv[j],"-s")==0 && argc>j) d=atoi(argv[++j]);
 		if(strcmp(argv[j],"-v")==0) v=1;
 		if(strcmp(argv[j],"-q")==0) q=1;
 	}
-	// p("d:%i,v=%i\n",d,v);
 	
-	srand(time(NULL));
-	o i[100000];
-	o is=sizeof(i)/sizeof(o);
-	is=is<d?is:d;
+	if(!q && d>=100000)
+	{
+		q=1;
+		p("automatically using qsort since size>=100000\n");
+	}
 	
-	// p("d:%i,v=%i,is=%i\n",d,v,is);
+	i=malloc(d*sizeof(int));
+	if(!i)
+	{
+		p("cannot allocate memory\n");
+		return 1;
+	}
 	
 	p("randomizing...\n");
-	for(int j=0;j<is;j++)i[j]=rand()%UINT_MAX;
+	for(int j=0;j<d;j++)i[j]=rand()%UINT_MAX;
 	
 	
-	// p("number of elements: %i\n",is);
+	// p("number of elements: %i\n",d);
 	if(v)
 	{
-		p("before: ");for(int j=0;j<is;j++)p("%i,",i[j]);
+		p("before: ");for(int j=0;j<d;j++)p("%i,",i[j]);
 		putc('\n',stdout);
 	}
 	
-	p("sorting %u elements...\n",is);
+	p("sorting %u elements...\n",d);
 	uint64_t cl=clock();
 	
 	//-q to use qsort
 	if(q)
-		qsort(i,is,sizeof(int),s2);
+		qsort(i,d,sizeof(int),s2);
 	else
-		for(int k=0;k<is-1;k++)
-			for(int j=0;j<is-1;j++)s(i+j);
+		for(int k=0;k<d-1;k++)
+			for(int j=0;j<d-1;j++)s(i+j);
 	
 	p("sorting done\n");
 	cl=clock()-cl; //'stop' clock
 	
 	if(v)
 	{
-		p("\nafter:  ");for(int j=0;j<is;j++)p("%i,",i[j]);
+		p("\nafter:  ");for(int j=0;j<d;j++)p("%i,",i[j]);
 		putc('\n',stdout);
 	}
 	
